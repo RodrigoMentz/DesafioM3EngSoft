b6d91d8135ceb579c1f51b8e37684b8f8e93cd4c
RodrigoMentz <95316880+RodrigoMentz@users.noreply.github.com>
2025-04-08 20:34:46 -0300

    Adiciona projeto inicial

---
+++ src/ItemParaCompra.java
@@ -0,0 +1,25 @@
+public class ItemParaCompra {
+    private String nome;
+    private boolean comprado;
+
+    public ItemParaCompra(String nome) {
+        this.nome = nome;
+        this.comprado = false; // padrão: não comprado
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean isComprado() {
+        return comprado;
+    }
+
+    public void marcarComoComprado() {
+        this.comprado = true;
+    }
+
+    public void desmarcarComoComprado() {
+        this.comprado = false;
+    }
+}
---
+++ src/ListaDeCompras.java
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaDeCompras {
+    private List<ItemParaCompra> itens;
+    private LocalDateTime dataAtualizacao;
+
+    public ListaDeCompras() {
+        this.itens = new ArrayList<>();
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    public List<ItemParaCompra> getItens() {
+        return itens;
+    }
+
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void adicionarItem(ItemParaCompra item) {
+        itens.add(item);
+        atualizarData();
+    }
+
+    public void removerItem(ItemParaCompra item) {
+        itens.remove(item);
+        atualizarData();
+    }
+
+    public void atualizarData() {
+        this.dataAtualizacao = LocalDateTime.now();
+    }
+
+    public void marcarItemComoComprado(String nome) {
+        for (ItemParaCompra item : itens) {
+            if (item.getNome().equalsIgnoreCase(nome)) {
+                item.marcarComoComprado();
+                atualizarData();
+                break;
+            }
+        }
+    }
+}
