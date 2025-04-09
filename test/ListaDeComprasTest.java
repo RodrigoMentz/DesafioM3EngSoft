aa39bd32934023e3b5f860ba81c4f5e9532ef0ae
RodrigoMentz <95316880+RodrigoMentz@users.noreply.github.com>
2025-04-08 21:31:31 -0300

    Adiciona teste unitario

--- test/ListaDeComprasTest.java
+++ test/ListaDeComprasTest.java
@@ -29,4 +29,13 @@ public class ListaDeComprasTest {
         lista.marcarItemComoComprado("Açúcar");
         assertTrue(lista.getItens().get(0).isComprado());
     }
+    
+    @Test
+    void testAdicionarItem2() {
+        ListaDeCompras lista = new ListaDeCompras();
+        ItemParaCompra item = new ItemParaCompra("Ovo");
+        lista.adicionarItem(item);
+        assertEquals(1, lista.getItens().size());
+        assertEquals("Leite", lista.getItens().get(0).getNome());
+    }
 }
